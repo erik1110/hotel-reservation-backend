af2e70ca5f0638b7e88b34a775290f37c14426de
erik1110 <oceanuheart@gmail.com>
2024-01-02 16:59:55 +0800

    [feat] add room api

--- src/features/room/room.controller.ts
+++ src/features/room/room.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { ApiErrorDecorator } from 'src/common/decorator/error/error.decorator';
 import { RoomService } from './room.service';
 import { Roles } from 'src/auth/decorators/roles.decorator';
-import { CreateRoomDto, CreateRoomSuccessDto } from './dto/room.dto';
+import { CreateRoomDto, CreateRoomSuccessDto, DeleteRoomSuccessDto, GetRoomSuccessDto, UpdateRoomSuccessDto } from './dto/room.dto';
+import { IsObjectIdPipe } from 'nestjs-object-id';
 
 
 @ApiTags('Admin/Rooms - 房型管理')
@@ -20,12 +21,45 @@ import { CreateRoomDto, CreateRoomSuccessDto } from './dto/room.dto';
 export class RoomController {
     constructor(private readonly roomService: RoomService) {}
 
+    @Get('')
+    @Roles('admin')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: '取得所有房型 Get all rooms' })
+    @ApiOkResponse({ type: GetRoomSuccessDto })
+    async getallNews(@Req() req: Request) {
+      return await this.roomService.getallRooms(req);
+    }
+
     @Post('')
     @Roles('admin')
     @HttpCode(HttpStatus.OK)
-    @ApiOperation({ summary: '新增最新房型 Add a latest room' })
+    @ApiOperation({ summary: '新增最新房型 Add a room' })
     @ApiOkResponse({ type: CreateRoomSuccessDto })
     async addNews(@Req() req: Request, @Body() createNewsDto: CreateRoomDto) {
       return await this.roomService.createRoom(req, createNewsDto);
+    }
+    @Put(':id')
+    @Roles('admin')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: '更新最新消息 Update latest news' })
+    @ApiOkResponse({ type: UpdateRoomSuccessDto })
+    async updateNews(
+      @Param('id', IsObjectIdPipe) id: string,
+      @Req() req: Request,
+      @Body() updateNewsDto: CreateRoomDto,
+    ) {
+      return await this.roomService.updateRoom(id, req, updateNewsDto);
+    }
+
+    @Delete(':id')
+    @Roles('admin')
+    @HttpCode(HttpStatus.OK)
+    @ApiOperation({ summary: '刪除最新消息 Delete latest news' })
+    @ApiOkResponse({ type: DeleteRoomSuccessDto })
+    async deleteNews(
+      @Param('id', IsObjectIdPipe) id: string,
+      @Req() req: Request,
+    ) {
+      return await this.roomService.deleteRoom(id, req);
     }
 }
